8481aaaa787f7f36a8d89e25edd5a557b48cfc3d
Aarni Koskela <akx@iki.fi>
2023-12-25 23:01:02 +0200

    Unify CodeFormer and GFPGAN restoration backends, use Spandrel for GFPGAN

--- webui.py
+++ webui.py
@@ -70,11 +70,11 @@ def webui():
         gradio_auth_creds = list(initialize_util.get_gradio_auth_creds()) or None
 
         auto_launch_browser = False
-        if os.getenv('SD_WEBUI_RESTARTING') != '1':
-            if shared.opts.auto_launch_browser == "Remote" or cmd_opts.autolaunch:
-                auto_launch_browser = True
-            elif shared.opts.auto_launch_browser == "Local":
-                auto_launch_browser = not cmd_opts.webui_is_non_local
+        # if os.getenv('SD_WEBUI_RESTARTING') != '1':
+        #     if shared.opts.auto_launch_browser == "Remote" or cmd_opts.autolaunch:
+        #         auto_launch_browser = True
+        #     elif shared.opts.auto_launch_browser == "Local":
+        #         auto_launch_browser = not cmd_opts.webui_is_non_local
 
         app, local_url, share_url = shared.demo.launch(
             share=cmd_opts.share,
